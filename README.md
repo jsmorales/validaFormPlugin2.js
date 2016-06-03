1fe5ea4fdea52f90bf957705fd13be7b6ff6c744
Johan Morales <jhellmetal2000@gmail.com>
2016-06-03 17:45:08 -0500

    Primer commit, del plugin validaFormPlugin2.js

---
+++ README.md
@@ -0,0 +1,12 @@
+#validaFormPlugin2.js
+
+Plugin que valida los controles de un formulario que tengan el atributo required = "true", añade estilos validate de bootstrap 3 para jsFramework.
+
+<h2>Atributos</h2>
+
+<b>min_width</b> = Valor de width minimo que debe llevar la barra de progreso, esta en "em" Ej: 45em.
+
+
+<h2>Set del Plugin</h2>
+
+$("selector").validaForm('min_width');
---
+++ validaFormPlugin2.js
@@ -0,0 +1,167 @@
+(function($){
+
+	var progreso = 0;
+	var style_progreso = 0;
+
+	$.fn.validaForm = function (min_width){
+
+		var selector = $(this);
+
+		console.log('ejecutando validaForm en '+this[0].id);
+
+		function crear_progress(){
+
+			if ($("#barra_progreso_div").length > 0) {
+				console.log('la barra de progreso ya está');
+			} else{				
+
+				console.log(selector)
+
+				$(selector).after('<div id="barra_progreso_div" class="progress">'+
+		                            '<div id="barra_progreso" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 0em;">'+
+		                                '0%'+
+		                            '</div>'+
+		                        '</div>');
+			};
+			
+		}
+
+		function contador(val_pro,val_style){
+			// body...
+			crear_progress();
+
+			progreso = progreso + val_pro;
+			style_progreso = style_progreso + val_style;
+
+			$("#barra_progreso").attr('aria-valuenow', progreso);
+			$("#barra_progreso").html(progreso+'%');
+			$("#barra_progreso").attr('style', 'min-width: '+style_progreso+'em;');
+
+			console.log('el progreso va en '+progreso);
+			if (progreso == 100) {
+				$("#barra_progreso").attr('class', 'progress-bar progress-bar-success');
+			};
+		}
+
+		function descontador(val_pro,val_style){
+			// body...
+			crear_progress();
+
+			progreso = progreso - val_pro;
+			style_progreso = style_progreso - val_style;
+
+			$("#barra_progreso").attr('aria-valuenow', progreso);
+			$("#barra_progreso").html(progreso+'%');
+			$("#barra_progreso").attr('style', 'min-width: '+style_progreso+'em;');
+
+			console.log('el progreso va en '+progreso);
+			if (progreso == 100) {
+				$("#barra_progreso").attr('class', 'progress-bar progress-bar-warning');
+			}else{
+				$("#barra_progreso").attr('class', 'progress-bar progress-bar-warning');
+			};
+		}
+
+		//----------------------------------------------------------------------------
+
+		var min_width_pro = 0;
+		var pro_form = 0;	
+		var cant_dom_req = 0;
+
+		$.each($(selector)[0], function(index, val) {
+
+			 console.log('index: '+index+' val: '+val)
+			 console.log(val.required)
+
+			 if (val.required == true) {
+			 	cant_dom_req = cant_dom_req + 1;
+			 };	
+
+		});
+
+		//console.log(cant_dom_req);		
+
+		function matematica_progress(min_width){
+
+			console.log(min_width);
+
+			min_width_pro = min_width / 100;
+			pro_form = 100 / cant_dom_req;
+
+			var val_fin = min_width_pro * pro_form;
+
+			return val_fin;
+		}
+
+		//pone todos en 0
+		$(".form-control").attr('pasoValida',0);
+
+		//--------------------------------------------------------------------
+
+		$(".form-control").change(function(event) {
+			/* Act on the event */
+			pro_form_cont = 100 / cant_dom_req;
+
+			var control_ant = '';
+			var control_act = '';
+
+			//identificar el control en el que esta
+			console.log($(this))
+			console.log($(this)[0]["value"])
+			console.log($(this)[0]["required"])
+			console.log($(this)[0]["type"])
+
+			if(($(this)[0]["value"]=="") && ($(this)[0]["required"] == true)){
+	            
+	            //pone el estilo rojo	            
+
+	            var padre = $(this).parent();
+
+	            var abuelo = padre.parent();
+
+	            console.log(abuelo);
+
+	            abuelo.attr('class', 'form-group has-error has-feedback');
+
+	            padre.children('span').remove();
+
+	            $(this).after('<span class="glyphicon glyphicon-remove form-control-feedback validaRemove" aria-hidden="true"></span>');
+	            
+				//-----------------------------------------------------------------------------------------------------
+
+
+	          }else if ( ($(this)[0]["value"]!="") && ($(this)[0]["required"] == true) ){
+	            
+	            //pone el estilo verde
+	            
+	            var padre = $(this).parent();
+
+	            var abuelo = padre.parent();
+
+	            //console.log(abuelo);
+
+	            abuelo.attr('class', 'form-group has-success has-feedback');
+
+	            console.log(padre.children('span').remove());
+
+	            $(this).after('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+
+	            //-----------------------------------------------------------------------------------------------------	          
+
+				if ( ( (progreso == 0) || (progreso < 100) ) && ($(this).attr('pasoValida') == 0) ) {
+					$(this).attr('pasoValida',1);
+					contador(pro_form_cont,matematica_progress(min_width));
+				}else if ( ( (progreso == 0) || (progreso < 100) ) && ($(this).attr('pasoValida') == 1) ) {
+					
+				};
+
+				//contador(pro_form_cont,matematica_progress(min_width));	
+				
+				//-----------------------------------------------------------------------------------------------------
+
+	          }
+
+		});
+	};
+
+})(jQuery);
